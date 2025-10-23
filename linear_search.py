3ad011f8f63ff9aff99ca731e18d0a879fd217cc
Surkhab Mundi <surkhabm1@gmail.com>
2025-10-22 22:38:23 -0600

    reviewed linear search

--- linear_search.py
+++ linear_search.py
@@ -1,2 +1,12 @@
 # LINEAR SEARCH : 0(n)^2
 # Search through array one by one, comparing values. 
+
+
+def linear_search(arr, target):
+    for i in range(len(arr) - 1):
+        if arr[i] == target:
+            return i
+
+target = 8
+arr = [1,2,3,8,4]
+print(linear_search(arr, target))
